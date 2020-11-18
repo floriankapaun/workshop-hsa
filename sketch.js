7e0f7a911b148d5628431ae7d8f193d5842e905a
Florian Kapaun <hello@florian-kapaun.de>
2020-11-18 14:19:05 +0100

    :boom: switched to ml5.js dcgan example

--- sketch.js
+++ sketch.js
@@ -1,46 +1,47 @@
-let video;
-let poseNet;
-let poses = [];
+let dcgan;
 
-//standard p5 structure
+// vector 1
+const a = [];
+// vector 2
+const b = [];
+// vector to interpolate between 1 and 2
+const c = [];
+
+let slider;
+
+function preload() {
+  dcgan = ml5.DCGAN('model/face/manifest.json');
+}
+
 function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.size(width, height);
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on("pose", function(results) {
-    poses = results;
-  });
-  video.hide();
-}
+  createCanvas(300, 300);
 
-
-function draw(){
-    background(255)
-    translate(width,0)
-    scale(-1,1);
-    tint(255,128)
-    image(video, 0, 0, width, height);
-    drawPoints();   
-}
-
-
-//Custom
-const modelLoaded = () => {
-  console.log('Model should be loaded')
-}
-
-const drawPoints = () => {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    const pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j += 1) {
-      const keypoint = pose.keypoints[j];
-      if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-      }
-    }
+  // create 2 arrays to hold random values for our latent vector
+  for (let i = 0; i < 128; i += 1) {
+    a[i] = random(-1, 1);
+    b[i] = random(-1, 1);
   }
+  slider = createSlider(0, 1, 0.5, 0.01);
+  slider.input(generate);
+
+  // generate an image on load
+  generate();
+}
+
+
+function generate() {
+  const amt = slider.value();
+  // fill the latent vector with the interpolation between a and b
+  for (let i = 0; i < 128; i += 1) {
+    c[i] = lerp(a[i], b[i], amt);
+  }
+  dcgan.generate(displayImage, c);
+}
+
+function displayImage(err, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  image(result.image, 0, 0, width, height);
 }
