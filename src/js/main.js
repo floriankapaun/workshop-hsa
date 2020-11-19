0b88643678a5fc7ac3c0817b14749a73085cc9c9
Florian Kapaun <hello@florian-kapaun.de>
2020-11-19 07:19:49 +0100

    :boom: switched handpose detection to tensorflow.js and webpack

---
+++ src/js/main.js
@@ -0,0 +1,95 @@
+const handpose = import(/* webpackPreload: true */ '@tensorflow-models/handpose');
+const tfjsBackendWebgl = import(/* webpackPreload: true */ '@tensorflow/tfjs-backend-webgl');
+const tf = import(/* webpackPreload: true */ '@tensorflow/tfjs-core');
+
+class HandposeDetection {
+    constructor(input, output) {
+        this.VIDEO_WIDTH = 500;
+        this.VIDEO_HEIGHT = this.VIDEO_WIDTH;
+        this.TF_BACKEND = 'webgl';
+        this.input = input;
+        this.output = output;
+        this.model = undefined;
+        this.ctx = undefined;
+    }
+
+    async init() {
+        await tfjsBackendWebgl;
+        await (await tf).setBackend(this.TF_BACKEND);
+        // Start webcam
+        await this.setupCamera();
+        // Play video
+        this.input.play();
+        this.input.width = this.VIDEO_WIDTH;
+        this.input.height = this.VIDEO_HEIGHT;
+        // Style canvas
+        this.output.width = this.VIDEO_WIDTH;
+        this.output.height = this.VIDEO_HEIGHT;
+        // NECESSARY?
+        // const canvasContainer = document.querySelector('.canvas-wrapper');
+        // canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+        // Setup context
+        this.ctx = this.output.getContext('2d');
+        this.ctx.translate(this.output.width, 0);
+        this.ctx.scale(-1, 1);
+        this.ctx.fillStyle = '#32EEDB';
+        // Setup tensorflow.js handpose model
+        this.model = await (await handpose).load();
+        // Start making predictions
+        this.renderPrediction();
+    }
+
+    async setupCamera() {
+        // Create new stream from camera
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                facingMode: 'user',
+                width: this.VIDEO_WIDTH,
+                height: this.VIDEO_HEIGHT,
+            },
+        });
+        // Set input source to created stream
+        this.input.srcObject = stream;
+        // Return promise that resolves if video is loaded
+        return new Promise((resolve) => {
+            this.input.onloadedmetadata = () => {
+                resolve(this.input);
+            };
+        });
+    }
+
+    async renderPrediction() {
+        // Make a prediction about the handposes
+        // the first prediction is taking very long.
+        const predictions = await this.model.estimateHands(this.input);
+        // Plott the input video as canvas background
+        // Clip the image and position the clipped part on the canvas
+        // ctx.drawImage(img,sx,sy,swidth,sheight,x,y,width,height)
+        this.ctx.drawImage(
+            this.input,
+            0, 0, this.VIDEO_WIDTH, this.VIDEO_HEIGHT,
+            0, 0, this.output.width, this.output.height,
+        );
+        // Plott the prediction onto the context
+        if (predictions.length > 0) {
+            const firstHand = predictions[0];
+            if (!firstHand.annotations
+                && !firstHand.annotations.indexFinger
+                && !firstHand.annotations.indexFinger.length) return false;
+            const pointer = firstHand.annotations.indexFinger[3];
+            this.ctx.beginPath();
+            this.ctx.arc(pointer[0], pointer[1], 10 /* radius */, 0, 2 * Math.PI);
+            this.ctx.fill();
+        }
+        // Render the next prediction
+        requestAnimationFrame(() => this.renderPrediction());
+    }
+}
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('output');
+
+const handposeDetection = new HandposeDetection(video, canvas);
+
+handposeDetection.init();
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+    const mode = env.mode;
+
+    const config = {
+        mode: mode,
+        entry: {
+            app: './src/js/main.js',
+            main: './src/css/main.css',
+        },
+        devServer: {
+            contentBase: './dist',
+            port: 9000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.handlebars$/,
+                    loader: 'handlebars-loader',
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        // 'postcss-loader',
+                        // 'sass-loader',
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Team Finger',
+                template: 'src/index.handlebars',
+                templateParameters: {
+                    title: 'Team Finger',
+                },
+                scriptLoading: 'defer',
+                hash: true,
+            }),
+        ],
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+    };
+
+    if (mode === 'development') { 
+        config.devtool = 'inline-source-map';
+    } else if (mode === 'production') {
+        config.plugins.push(new MiniCssExtractPlugin());
+    }
+
+    return config;
+};
